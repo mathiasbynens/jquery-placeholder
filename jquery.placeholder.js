4356d42eb6c88258ef977e541b9bc94cbebac074
Erik Montes <zomberik@gmail.com>
2014-12-29 08:34:50 -0800

    bumping version to 2.1.0

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/placeholder v2.0.9 by @mathias */
+/*! http://mths.be/placeholder v2.1.0 by @mathias */
 (function(factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD
