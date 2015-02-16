9cbe6fad5284742dd1eb4e9533e30ab44c842068
Callum Macrae <callum@macr.ae>
2015-02-16 15:30:24 +0000

    Added npm support

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -3,6 +3,8 @@
 	if (typeof define === 'function' && define.amd) {
 		// AMD
 		define(['jquery'], factory);
+	} else if (typeof module === 'object' && exports) {
+		factory(require('jquery'));
 	} else {
 		// Browser globals
 		factory(jQuery);
