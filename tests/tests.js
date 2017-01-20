1ad6dacb0bbf717e95d4cba5dd05a3e933f93cd4
Mathias Bynens <mathias@qiwi.be>
2017-01-20 09:06:07 +0100

    Update QUnit.js

--- tests/tests.js
+++ tests/tests.js
@@ -1,88 +1,90 @@
 (function($) {
 
-	module('jQuery#placeholder');
+	QUnit.module('jQuery#placeholder');
 
-	test('caches results of feature tests', function() {
-		strictEqual(typeof $.fn.placeholder.input, 'boolean', '$.fn.placeholder.input');
-		strictEqual(typeof $.fn.placeholder.textarea, 'boolean', '$.fn.placeholder.textarea');
+	var test = QUnit.test;
+
+	test('caches results of feature tests', function(assert) {
+		assert.strictEqual(typeof $.fn.placeholder.input, 'boolean', '$.fn.placeholder.input');
+		assert.strictEqual(typeof $.fn.placeholder.textarea, 'boolean', '$.fn.placeholder.textarea');
 	});
 
 	if ($.fn.placeholder.input && $.fn.placeholder.textarea) {
 		return;
 	}
 
-	var testElement = function($el) {
+	var testElement = function($el, assert) {
 
 		var el = $el[0];
 		var placeholder = el.getAttribute('placeholder');
 
 		deepEqual($el.placeholder(), $el, 'should be chainable');
 
-		strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok($el.hasClass('placeholder'), 'should have `placeholder` class');
 
 		// test on focus
 		$el.focus();
-		strictEqual(el.value, '', '`value` should be the empty string on focus');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, '', '`value` should be the empty string on focus');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok(!$el.hasClass('placeholder'), 'should not have `placeholder` class on focus');
 
 		// and unfocus (blur) again
 		$el.blur();
 
-		strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok($el.hasClass('placeholder'), 'should have `placeholder` class');
 
 		// change the value
 		$el.val('lorem ipsum');
-		strictEqual($el.prop('value'), 'lorem ipsum', '`$el.val(string)` should change the `value` property');
-		strictEqual(el.value, 'lorem ipsum', '`$el.val(string)` should change the `value` attribute');
+		assert.strictEqual($el.prop('value'), 'lorem ipsum', '`$el.val(string)` should change the `value` property');
+		assert.strictEqual(el.value, 'lorem ipsum', '`$el.val(string)` should change the `value` attribute');
 		ok(!$el.hasClass('placeholder'), '`$el.val(string)` should remove `placeholder` class');
 
 		// and clear it again
 		$el.val('');
-		strictEqual($el.prop('value'), '', '`$el.val("")` should change the `value` property');
-		strictEqual(el.value, placeholder, '`$el.val("")` should change the `value` attribute');
+		assert.strictEqual($el.prop('value'), '', '`$el.val("")` should change the `value` property');
+		assert.strictEqual(el.value, placeholder, '`$el.val("")` should change the `value` attribute');
 		ok($el.hasClass('placeholder'), '`$el.val("")` should re-enable `placeholder` class');
 
 		// make sure the placeholder property works as expected.
-		strictEqual($el.prop('placeholder'), placeholder, '$el.prop(`placeholder`) should return the placeholder value');
+		assert.strictEqual($el.prop('placeholder'), placeholder, '$el.prop(`placeholder`) should return the placeholder value');
 		$el.prop('placeholder', 'new placeholder');
-		strictEqual($el.prop('placeholder'), 'new placeholder', '$el.prop(`placeholder`, <string>) should set the placeholder value');
-		strictEqual($el.value, 'new placeholder', '$el.prop(`placeholder`, <string>) should update the displayed placeholder value');
+		assert.strictEqual($el.prop('placeholder'), 'new placeholder', '$el.prop(`placeholder`, <string>) should set the placeholder value');
+		assert.strictEqual($el.value, 'new placeholder', '$el.prop(`placeholder`, <string>) should update the displayed placeholder value');
 		$el.prop('placeholder', placeholder);
 	};
 
-	test('emulates placeholder for <input type=text>', function() {
-		testElement( $('#input-type-text') );
+	test('emulates placeholder for <input type=text>', function(assert) {
+		testElement( assert, $('#input-type-text') );
 	});
 
-	test('emulates placeholder for <input type=search>', function() {
-		testElement( $('#input-type-search') );
+	test('emulates placeholder for <input type=search>', function(assert) {
+		testElement( assert, $('#input-type-search') );
 	});
 
-	test('emulates placeholder for <input type=email>', function() {
-		testElement( $('#input-type-email') );
+	test('emulates placeholder for <input type=email>', function(assert) {
+		testElement( assert, $('#input-type-email') );
 	});
 
-	test('emulates placeholder for <input type=url>', function() {
-		testElement( $('#input-type-url') );
+	test('emulates placeholder for <input type=url>', function(assert) {
+		testElement( assert, $('#input-type-url') );
 	});
 
-	test('emulates placeholder for <input type=tel>', function() {
-		testElement( $('#input-type-tel') );
+	test('emulates placeholder for <input type=tel>', function(assert) {
+		testElement( assert, $('#input-type-tel') );
 	});
 
-	test('emulates placeholder for <input type=tel>', function() {
-		testElement( $('#input-type-tel') );
+	test('emulates placeholder for <input type=tel>', function(assert) {
+		testElement( assert, $('#input-type-tel') );
 	});
 
-	test('emulates placeholder for <input type=password>', function() {
+	test('emulates placeholder for <input type=password>', function(assert) {
 		var selector = '#input-type-password';
 
 		var $el = $(selector);
@@ -90,15 +92,15 @@
 
 		var placeholder = el.getAttribute('placeholder');
 
-		strictEqual($el.placeholder(), $el, 'should be chainable');
+		assert.strictEqual($el.placeholder(), $el, 'should be chainable');
 
 		// Re-select the element, as it gets replaced by another one in some browsers
 		$el = $(selector);
 		el = $el[0];
 
-		strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok($el.hasClass('placeholder'), 'should have `placeholder` class');
 
 		// test on focus
@@ -108,9 +110,9 @@
 		$el = $(selector);
 		el = $el[0];
 
-		strictEqual(el.value, '', '`value` should be the empty string on focus');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, '', '`value` should be the empty string on focus');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok(!$el.hasClass('placeholder'), 'should not have `placeholder` class on focus');
 
 		// and unfocus (blur) again
@@ -120,15 +122,15 @@
 		$el = $(selector);
 		el = $el[0];
 
-		strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
-		strictEqual($el.prop('value'), '', 'propHooks works properly');
-		strictEqual($el.val(), '', 'valHooks works properly');
+		assert.strictEqual(el.value, placeholder, 'should set `placeholder` text as `value`');
+		assert.strictEqual($el.prop('value'), '', 'propHooks works properly');
+		assert.strictEqual($el.val(), '', 'valHooks works properly');
 		ok($el.hasClass('placeholder'), 'should have `placeholder` class');
 
 	});
 
-	test('emulates placeholder for <textarea></textarea>', function() {
-		testElement( $('#textarea') );
+	test('emulates placeholder for <textarea></textarea>', function(assert) {
+		testElement( assert, $('#textarea') );
 	});
 
 }(jQuery));
